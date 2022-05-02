9d184899ca460334f8cfc35cf170c4f56c241d7d
beninsydney <benodev@protonmail.com>
2022-05-02 18:23:42 +1000

    SSL mode

--- src/storage-postgresql.js
+++ src/storage-postgresql.js
@@ -2,8 +2,22 @@ const { Sequelize } = require('sequelize')
 const Log = require('./log.js')('sequelize-postgresql')
 
 module.exports = async () => {
-  const sequelize = new Sequelize(process.env.POSTGRESQL_DATABASE_URL, {
-    dialect: 'sqlite',
+  let url = process.env.POSTGRESQL_DATABASE_URL
+  const sslModeRequiredIndex = url.indexOf('?sslmode=require')
+  let dialectOptions
+  if (sslModeRequiredIndex > -1) {
+    url = url.substring(0, sslModeRequiredIndex)
+    dialectOptions = {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      keepAlive: true
+    }
+  }
+  const sequelize = new Sequelize(url, {
+    dialect: 'postgres',
+    dialectOptions,
     logging: (sql) => {
       return Log.info(sql)
     },
